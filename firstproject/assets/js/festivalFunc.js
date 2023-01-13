35204f1df1f7cdb9d1e8af906c405ffd646b9d63
IANAHB0517 <fromtheedge0517@gmail.com>
2023-01-13 18:19:40 +0900

    goott_25

--- firstproject/assets/js/festivalFunc.js
+++ firstproject/assets/js/festivalFunc.js
@@ -1,3 +1,14 @@
-let baseUrl = `http://apis.data.go.kr/B551011/KorService/searchFestival?MobileOS=ETC&MobileApp=tourApp&serviceKey=4F6HAPDoVB3FHDYvgt%2Fvmfa1NF7xD6m97UFCrUuzR6VbOhVXHSp24vutnt61xVrMNdMmvWpINT45qmIoTvwv4A%3D%3D&`;
-
-// getParameter("contentTypeId");
+function ajaxCall(baseUrl) {
+  $.ajax({
+    url: baseUrl,
+    type: "GET",
+    dataType: "json",
+    async: false,
+    success: function (data) {
+      parsing(data);
+      console.log(data);
+    },
+    error: function () {},
+    complete: function () {},
+  });
+}
