e186c475ec57d98acdf10f555ebc4c9ccc2004fe
IANAHB0517 <formtheedge0517@gmail.com>
2023-02-08 02:50:22 +0900

    review_230208_2

--- java/FishBreadOrder/src/weblsj/FishBreadBox.java
+++ java/FishBreadOrder/src/weblsj/FishBreadBox.java
@@ -11,6 +11,7 @@ public class FishBreadBox {
 	public FishBreadBox(String boxNo, int cnt) {
 		this.boxNo = boxNo;
 		this.breadList = new FishBread[cnt];
+		autoInput();
 		for (int i = 0; i < cnt; i++) {
 			 this.totalPrice += breadList[i].getPrice();			
 		}
@@ -19,9 +20,42 @@ public class FishBreadBox {
 	
 	//메서드
 	//getter
+	public String getBoxNo() {
+		return boxNo;
+	}
+	
+	public FishBread[] getBreadList() {
+		return breadList;
+	}
+	
+	public int getTotalPrice() {
+		return totalPrice;
+	}
 	
 	//setter
-	//toString
 	
+	public void setBoxNo(String boxNo) {
+		this.boxNo = boxNo;
+	}
+	
+	//toString
+	public String toString() {
+		String result = null; 
+		result = this.boxNo + "박스에는 "; 
+		int breadCnt = 0;
+	for (FishBread f : breadList) {
+		result += f.getSock() + "붕어빵, ";
+		breadCnt++;
+	}
+	result += "모두 합해 총" + breadCnt + "개의 붕어빵이 들어있고 가격은 " + this.getTotalPrice() + "입니다.";
+		return result;
+	}
+	
+public void autoInput() {
+	for (int i = 0; i < breadList.length; i++) {
+		this.breadList[i] = new FishBread("팥", 10, 1000);
+		
+	}
+}
 	
 }
--- java/FishBreadOrder/src/weblsj/FishBreadOrder.java
+++ java/FishBreadOrder/src/weblsj/FishBreadOrder.java
@@ -3,8 +3,11 @@ package weblsj;
 public class FishBreadOrder {
 
 	public static void main(String[] args) {
+		FishBreadBox order1 = new FishBreadBox("b001",4 );
 		
+		System.out.println(order1.toString());
 
 	}
 
+	
 }
