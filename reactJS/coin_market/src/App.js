8470272717041224402836c3818af5bced676ac5
lsj <fromtheedge0517@gmail.com>
2023-05-31 16:41:21 +0900

    goott_116

--- reactJS/coin_market/src/App.js
+++ reactJS/coin_market/src/App.js
@@ -6,7 +6,11 @@ import "./App.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
+  const [yourDallar, setYourDallar] = useState(0);
+  const [coinPrice, setCoinPrice] = useState(0);
+  const [selectedCoinIndex, setSelectedCoinIndex] = useState("");
+  const [coinName, setCoinName] = useState("");
 
   // App 컴포넌트가 매번 render되더라도 한번만 호출하여 api에서 json을 가져와 coins에 넣어준다
   useEffect( () => {
@@ -17,11 +21,54 @@ function App() {
     console.log(coins);
   }, []);
 
+  const inputDollar = (e) => {
+    console.log(e.target.value);
+    setYourDallar(e.target.value);
+  }
+
+  const changeCoin = (e) => {
+    let selectTag = e.target;
+    let selectedIndex = selectTag.selectedIndex -1; // 선택한 배열의 index 값
+    console.log(coins[selectedIndex].name);
+    setCoinName(coins[selectedIndex].name);
+    
+    if (e.target.value != -1){
+      console.log("선택한 코인 가격 : " + e.target.value);
+      setCoinPrice(e.target.value);
+      console.log("선택한 코인 Index : " +  selectedIndex);
+      setSelectedCoinIndex(selectedIndex);
+
+    }
+  }
+
   return (
    <div className='container'>
     <h1>Coin Market</h1>
     <div>
       {loading ? <h4>Loading...</h4> : null}
+    </div>
+
+    <div>
+      <input type="number" className="form-control" placeholder="보유중인 달러" onChange={inputDollar}/>
+
+    </div>
+    
+    <div>
+      <select className="form-select form-select-lg" onChange={changeCoin}>
+        <option key ={-1} value={-1}>코인을 선택하세요</option>
+        {coins.map((coin) => (
+          <option key={coin.id} value={coin.quotes.USD.price.toFixed(2)}>{coin.symbol}</option>
+        ))}
+      </select>
+    </div>
+
+    <hr />
+
+    <div>
+      {(yourDallar > 0 && coinPrice != -1) ? 
+        <h3>보유중인 ${yourDallar}로 구매 가능한 {coinName} 코인의 갯수는 {(yourDallar / coinPrice).toFixed(2)} 개 입니다.</h3>
+        // <h3>보유중인 ${yourDallar}로 구매 가능한 {coins[selectedCoinIndex].id} {coinName} 코인의 갯수는 {(yourDallar / coinPrice).toFixed(2)} 개 입니다.</h3>
+      : null}
     </div>
 
     <div className="lastUpdate">
